ce7731aa3ad0eb49cd83d1054b7199a9183e259f
Jan-PieterK <jpknol@icloud.com>
2022-02-21 19:51:59 +0100

    Lesson 3 Variables, expressions and statements

---
+++ lesson3/course.py
@@ -0,0 +1,31 @@
+# Exercise 2.2
+x = input('Enter your name: ')
+print('hello', x)
+
+# Exercise 2.3
+xh = input("enter Hours: ")
+xr = input("enter Rate: ")
+xp = float(xh) * float(xr)
+print("pay:", xp)
+
+# Exercise 2.4
+width = 17
+height = 12.0
+
+a = width // 2
+print(a)
+
+b = width / 2.0
+print(b)
+
+c = height / 3
+print(c)
+
+d = 1 + 2 * 5
+print(d)
+
+# Exercise 2.5
+celcius_temp_str = input("enter temputure in Celsius: ")
+celcius_temp_int = float(celcius_temp_str)
+fahrenheit_temp = celcius_temp_int * 9 / 5 + 32
+print(fahrenheit_temp)
